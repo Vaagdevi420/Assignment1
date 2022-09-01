a6c083ef3dc643d8c6073b3d3db21aa3c9016ced
Vaagdevi420 <112424562+Vaagdevi420@users.noreply.github.com>
2022-09-01 18:03:27 -0500

    Create question5.py

---
+++ question5.py
@@ -0,0 +1,10 @@
+import math
+r = 30
+pi = 3.14
+_area_of_circle_ = pi * r * r
+print("area of circle = %2f" % _area_of_circle_)
+_circum_of_circle= 2 * pi * r
+print("Circumference of circle = %2f" %_circum_of_circle)
+radius = float(input("enter radius of circle:"))
+area = pi * radius * radius
+print("area is = %2f" % area)

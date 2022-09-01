22597300c5f6b93ce5bb160803f72da6f1b7b32d
Vaagdevi420 <112424562+Vaagdevi420@users.noreply.github.com>
2022-09-01 18:01:24 -0500

    Create question3.py

---
+++ question3.py
@@ -0,0 +1,7 @@
+brother = ('bannu','vaibhav')
+sister = ('vinnu','vaagdevi')
+siblings = brother + sister
+print(siblings)
+siblings+=('madhavi','sv sesha giri rao')
+family_members = siblings 
+print(family_members)
